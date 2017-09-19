2266e89009709f63f40ab80989ce97b78db0ffd2
Pradhisha <pradhisha@gmail.com>
2017-09-19 15:30:34 +1000

    Basic Design Template

--- StockAidMain/src/app/app.module.ts
+++ StockAidMain/src/app/app.module.ts
@@ -3,14 +3,123 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+//material
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+ 
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+} from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
+import { HttpModule } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+
+
+//for primeng
+import { ChartModule } from 'primeng/primeng';
+
+
+@NgModule({
+  exports: [
+    ChartModule
+  ]
+})
+export class PrimeModule { }
+//end for primeng
+
+@NgModule({
+  exports: [
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+    FlexLayoutModule
+    
+  ]
+})
+export class MaterialModule { }
+//material done
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+   
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    MdNativeDateModule,
+    ReactiveFormsModule,
+    PrimeModule,
+    FlexLayoutModule
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
