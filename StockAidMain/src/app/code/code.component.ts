fb8d6350d138e7e0ff2b17e33f887a8533605935
Pradhisha <pradhisha@gmail.com>
2017-09-19 20:48:27 +1000

    Http Success!

--- StockAidMain/src/app/code/code.component.ts
+++ StockAidMain/src/app/code/code.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {Http,Response} from '@angular/http';
 
 
 @Component({
@@ -9,12 +10,33 @@ import {FormControl} from '@angular/forms';
 })
 export class AutocompleteSimpleExample {
   
+  constructor(private http:Http){}
+  symbol='';
+  companyName='';
+  latestPrice='';
+  primaryExchange='';
+  sector='';
+
+  searchStock(){
+    this.http.get('https://api.iextrading.com/1.0/stock/'+this.symbol+'/quote')
+    .subscribe(
+      (res:Response)=>{
+        const s= res.json();
+        console.log(s);
+        this.companyName=s.companyName;
+        this.latestPrice=s.latestPrice;
+        this.primaryExchange=s.primaryExchange;
+        this.sector=s.sector;
+      }
+    )
+
+  }
     myControl: FormControl = new FormControl();
   
     options = [
-      'Google',
-      'Apple',
-      'IBM'
+      'GE',
+      'Aapl',
+      'AA'
      ];
   
   }
