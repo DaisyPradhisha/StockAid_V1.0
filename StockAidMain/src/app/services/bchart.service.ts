014590f602d0747e8af5cec38539778ff5aa0f8d
AuslinPJ <31402534+AuslinPJ@users.noreply.github.com>
2017-10-15 14:50:53 +0530

    resolve intro

--- StockAidMain/src/app/services/bchart.service.ts
+++ StockAidMain/src/app/services/bchart.service.ts
@@ -3,15 +3,22 @@ import { Input} from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from "rxjs/Observable";
+import { Chart } from "../structure/chart";
 
 @Injectable()
 export class BchartService {
   constructor(private http:Http) { }
+
     getchart(sym:string,interval:string){
       console.log(sym);
       console.log(interval);
               return this.http.get('https://api.iextrading.com/1.0/stock/'+sym+'/chart/'+interval).map(res=>res.json());
          }
+         getchartSlowly(): Promise<any> {
+          return new Promise(resolve => {
+                    setTimeout(() => resolve(this.getchart('','')), 1000);
+      });
+    }
 
 
 }
